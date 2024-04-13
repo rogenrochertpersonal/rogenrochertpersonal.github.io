6e750cd22fa67a133001623a7c1471a0c01d0787
rogenrochertpersonal <166953731+rogenrochertpersonal@users.noreply.github.com>
2024-04-13 22:35:42 +0200

    Update lovesserver.py

--- lovesserver.py
+++ lovesserver.py
@@ -1,4 +1,20 @@
 import scratchattach, os, time
+import requests
+
+headers = {
+    'Accept-Language': 'en, en;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Origin': 'https://scratch.mit.edu',
+    'Pragma': 'no-cache',
+    'Referer': 'https://scratch.mit.edu/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'TE': 'trailers',
+    'X-Token': '236e5101158f44a68dc62f71291ddd0f:ya_cJ5XM27VwzKL3TI09KXIw-dI',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
+}
 
 SESSION_ID = os.getenv("SESSION_ID")
 USERNAME = "StrangeIntensity"
@@ -14,10 +30,10 @@ last_stats = None
 while time.time() < end_time:
     time.sleep(5)
     
-    project.update()
-    loves = project.loves
-    faves = project.favorites
-    views = project.views
+    stats = requests.get('https://api.scratch.mit.edu/projects/1000531175', headers=headers).json()["stats"]
+    loves = stats["loves"]
+    faves = stats["favorites"]
+    views = stats["views"]
     
     if last_stats == (views, loves, faves):
         pass#continue
